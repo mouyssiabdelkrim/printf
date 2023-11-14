ef999be27ee3e52eb888a13811bff7087e1a6df3
Oloishorua <jacoboloishorua@gmail.com>
2023-11-15 01:52:52 +0300

    function that prints uppercase in hexadecimal notation

---
+++ print_Hexadecimal_Extra.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_hexadecimal_extra - prints an hexgecimal number.
+ * @numb: arguments.
+ * Return: counter.
+ * Done by Jacob and Abdelkrim
+ */
+int print_hexadecimal_extra(unsigned long int numb)
+{
+        long int k;
+        unsigned long int temp_no = numb;
+        long int *arr;
+        long int counter = 0;
+
+        while (numb / 16 != 0)
+        {
+                numb /= 16;
+                counter++;
+        }
+        counter++;
+        arr = malloc(counter * sizeof(long int));
+
+        for (k = 0; k < counter; k++)
+        {
+                arr[k] = temp_no % 16;
+                temp_no = temp_no / 16;
+        }
+        for (k = counter - 1; k >= 0; k--)
+        {
+                if (arr[k] > 9)
+                        arr[k] = arr[k] + 39;
+                _putchar(arr[k] + '0');
+        }
+        free(arr);
+        return (counter);
+}
